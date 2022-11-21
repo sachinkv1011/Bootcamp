b269ac6661cb67f70a95ef6c4f702e9cb6cb6863
SACHIN K V <sachin.kv.1011@gmail.com>
2022-11-21 10:12:54 +0530

    Initial commit

---
+++ src/MenuDriven.java
@@ -0,0 +1,9 @@
+public class MenuDriven {
+    public static void main(String[] args) {
+        System.out.println("1.Addition");
+        System.out.println("2.Submission");
+        System.out.println("3.Muiltiplication");
+        System.out.println("4.Division");
+
+    }
+}
